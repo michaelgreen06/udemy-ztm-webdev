9e24e3287eb58092db16cfc68f1d715520d8f882
michael green <michaelgreen06@gmail.com>
2021-11-04 04:13:31 +0100

    learning functions!

---
+++ section11-js/exercise4/script.js
@@ -0,0 +1,27 @@
+function hello(){
+  console.log("Say Hello");
+}
+
+hello();
+
+var sayBye=function(){
+  console.log("BYEEEE Felicia!");
+}
+
+sayBye();
+
+function sign(song){
+  console.log(song);
+}
+
+sign("ladeda");
+sign("tatateteta");
+
+function multiply(a,b){
+    if (a>10 || b>10) {
+      return "that's childs play";
+    }
+else {
+  return a*b;
+}
+}
