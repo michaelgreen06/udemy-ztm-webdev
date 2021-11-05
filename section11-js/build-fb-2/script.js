9697d4ef7160dffdcb86660d30be36e3cceb3e1c
michael green <michaelgreen06@gmail.com>
2021-11-05 21:00:59 +0100

    loopsgit add *

---
+++ section11-js/build-fb-2/script.js
@@ -0,0 +1,51 @@
+var database=[
+  {
+    username:"andrei",
+    password:"supersecret"
+  },
+  {
+    username:"sally",
+    password:"123"
+  },
+  {
+    username:"ingrid",
+    password:"777"
+  },
+];
+
+var newsfeed=[
+  {
+    username:"boby",
+    timeline: "so tired from all that learning"
+  },
+  {
+    username:"sally",
+    timeline:"Javascript is sooooo cool"
+  },
+  {
+    username:"Mitch",
+    timeline: "can't wait to code cool things w/ new knowledge"
+  },
+];
+
+function signIn(username, password){
+  if (username===database[0].username &&
+    password===database[0].password){
+      console.log(newsfeed);
+    }
+    else{
+      alert("sorry, wrong username & password");
+    }
+}
+//my guess at the proper signin loop
+database.forEach(function(username,password)){
+  if (username===database[i].username &&
+    password===database[i].password){
+      console.log(newsfeed);
+}
+
+var userNamePrompt=prompt("What's your username?");
+var passwordPrompt=prompt("what's your password?");
+
+
+signIn(userNamePrompt,passwordPrompt);
--- section11-js/loops/loops.js
+++ section11-js/loops/loops.js
@@ -1,4 +1,4 @@
-//for loops
+//for loops - most common loop
 var todos=[
   "clean room",
   "brush teeth",
@@ -9,5 +9,30 @@ var todos=[
 
 var todosLength=todos.length;
 for(var i=0; i<todosLength; i++){
-  todos.pop();
+  console.log(todos[i],i);
 }
+//
+// //while loops
+// var counterOne=10;
+// while (counterOne>0){
+//   console.log(counterOne);
+//   counterOne--
+// }
+
+//do while loop
+// var counterTwo=10;
+// do{
+//   console.log(counterTwo);
+//   counterTwo--;
+// }while (counterTwo>0);
+
+//forEach loop - only for arrays?
+todos.forEach(function(todo,i) {
+  console.log(todo,i);
+})
+
+//foreach 2nd iteration
+function logToDos(todo,i){
+  console.log(todo,i);
+}
+todos.forEach(logToDos);
