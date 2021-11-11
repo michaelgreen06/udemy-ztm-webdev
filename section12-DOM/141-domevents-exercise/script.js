fb8dfba0ccff81a106ff44c574254ed159fc7bcf
michael green <michaelgreen06@gmail.com>
2021-11-11 00:20:57 +0000

    inspecting the solutions of others

--- section12-DOM/141-domevents-exercise/script.js
+++ section12-DOM/141-domevents-exercise/script.js
@@ -2,10 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var lii = document.querySelectorAll("li");
-//I get an error whenever I define a variable as something that returns
-//more than one result. This happens w/ the below code:
-// var lii = document.querySelectorAll("li");
-// var lii = document.getElementsByTagName("li");
+
 
 function inputLength() {
 	return input.value.length;
@@ -16,6 +13,7 @@ function createListElement() {
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+
 }
 
 function addListAfterClick() {
@@ -36,24 +34,12 @@ function addDoneClass(){
 				this.classList.toggle("done");
 		 });}}
 
-//copied & pasted above function hoping to do a callback because it seems cleaner
- // function addDoneClass(){
- // 	for (var index = 0; index <lii.length; index++){
- // 		 lii[index].classList.toggle("done");
- // 		 };   }
-
-	// document.querySelector("lii").classList.toggle("done");
-	// ul.classList.toggle("done");
-	// lii.classList.toggle("done");
-
 
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 addDoneClass();
 
-// my 1st guess at selecting li's
-// ul.addEventListener("click", addDoneClass);
-// lii.addEventListener("click", addDoneClass);
+
 
 
 //acquired from: https://www.tutorialspoint.com/how-to-remove-li-elements-on-button-click-in-javascript
