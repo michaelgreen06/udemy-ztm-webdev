664d5bfa538bb90bc546b4abc9b836a4b5ad9ff0
michael green <michaelgreen06@gmail.com>
2021-11-18 18:40:21 +0000

    finished ex 141! Could go back and re-try to solidify if desired

--- section12-DOM/141-domevents-exercise/pt4.js
+++ section12-DOM/141-domevents-exercise/pt4.js
@@ -12,16 +12,33 @@ function adddel(){
 		var ddbtn=document.createElement("button");
 		gli[i].appendChild(ddbtn);
 		ddbtn.appendChild(document.createTextNode("Delete"));
+		ddbtn.addEventListener("click",remove1);
+		// ddbtn.onclick=function(){
+		// 	gli[i].remove();
+		// }
+		//this below code was suggested by ascii and also works!
+		// gli[i].appendChild(document.createElement("button")).appendChild(document.createTextNode("Delete"));
 		// original code. top line works. bottom doesnt
 		//ascii says it's because .createTextNode() method is part of the document
 		// object, not the element you are creating. You will need to refer to the
 		// element you are creating and use .appendChild(document.createTextNode(someText)) on it.
 		// gli[i].appendChild(document.createElement("button"));
 		// gli[i].appendChild(document.createTextNode("Delete"));
+
 	}
 }
 
 adddel();
+
+// function remove(){
+// 	querySelectorAll("button")
+// }
+
+function remove1(){
+	event.target.parentElement.remove();
+	}
+
+
 
 function strike (){
 	for (let i=0; i<gli.length; i++){
@@ -32,6 +49,8 @@ function addDoneClass(){
 	this.classList.toggle("done");
 }
 strike();
+
+
 
 function createListElement() {
 	var li = document.createElement("li");
