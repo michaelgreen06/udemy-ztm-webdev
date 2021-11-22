51e4cdae1019153a4d8eba9e4a3a7bd771a475ac
michael green <michaelgreen06@gmail.com>
2021-11-22 21:26:39 +0000

    finished additional reqs for bg generator. My random button is janky. could be improved with a loop.

--- section12-DOM/143-background-generator/script.js
+++ section12-DOM/143-background-generator/script.js
@@ -2,6 +2,20 @@ var css=document.querySelector("h3");
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var body=document.getElementById("gradient");
+var random1=document.getElementById("random1");
+var random2=document.getElementById("random2");
+var c1= Math.floor(Math.random() * 256);
+var c2= Math.floor(Math.random() * 256);
+var c3= Math.floor(Math.random() * 256);
+var c4= Math.floor(Math.random() * 256);
+var c5= Math.floor(Math.random() * 256);
+var c6= Math.floor(Math.random() * 256);
+
+function init(){
+  css.textContent="linear-gradient(to right, #ff0000, #ffff00);";
+}
+
+init();
 
 function setGradient(){
   body.style.background=
@@ -17,3 +31,27 @@ function setGradient(){
 color1.addEventListener("input",setGradient);
 
 color2.addEventListener("input",setGradient);
+
+
+
+function randoGradient(){
+  body.style.background=
+    "linear-gradient(to right, rgb("
+     + c1
+     + ", "
+     + c2
+     + ", "
+     + c3
+     +"),"
+     +"rgb("
+     + c4
+     + ", "
+     + c5
+     + ", "
+     + c6
+     +"))";
+
+    css.textContent=body.style.background + ";";
+}
+
+random1.addEventListener("click",randoGradient);
