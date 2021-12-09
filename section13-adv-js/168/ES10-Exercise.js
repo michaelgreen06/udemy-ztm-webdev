95592fe6ae8867007f34c5eb92a9cc08131c2634
michael green <michaelgreen06@gmail.com>
2021-12-09 19:42:55 +0000

    still working on ES10 exercises

--- section13-adv-js/168/ES10-Exercise.js
+++ section13-adv-js/168/ES10-Exercise.js
@@ -30,7 +30,9 @@ const usersArray=Object.entries(users);
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2
 //-- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+const usersArray=Object.entries(users);
+usersArray.map((user,id)=>id*2);
 
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to
