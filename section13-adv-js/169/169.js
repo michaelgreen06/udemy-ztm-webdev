254be74ddf9e4fdf459394fa13a6c84455b8ba30
michael green <michaelgreen06@gmail.com>
2022-01-14 14:35:36 +0000

    learnind about advanced loops

---
+++ section13-adv-js/169/169.js
@@ -0,0 +1,26 @@
+const basket = ['apples','oranges','grapes'];
+const detailedBasket={
+  apples:2,
+  oranges:10,
+  grapes:1
+}
+//1 for loop we already know
+for (var i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+//2
+basket.forEach((item) => {
+  console.log(item);
+});
+
+//for of loop
+//iteratable items - arrays, strings
+for (item of basket){
+  console.log(item);
+}
+
+//for in loop - works w/ objects
+//w/ objects we don't iterate we enumerate!
+for (item in detailedBasket){
+  console.log(item);
+}
