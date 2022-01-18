7ee09f89852d8971590a3b05e6ef5cae5e398543
michael green <michaelgreen06@gmail.com>
2022-01-18 23:31:11 +0000

    solved #1 w/ jankiness then viewed andrei's answer

--- section13-adv-js/170/advanced-looping-exercise.js
+++ section13-adv-js/170/advanced-looping-exercise.js
@@ -34,29 +34,57 @@ const array3 = [] // should return 0
 
 function biggestNumberInArray(arr) {
 //maybe use sort?
-  arr.sort((a, b) => b - a);
-  console.log(arr);
+//this sort arrangement didn't work
+//looks like it should be a-b not b-a
+  // arr.sort((a, b) => b - a);
+  // console.log(arr);
 }
 //maybe use filter?
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 //maybe not though because then I wouldn't use a loop
 
 //maybe I use an if statement if i>i+1 return i else return i+1
+//this works when the array has been sorted.
+
+//my JANKY solution works for array & arry2 but not array3
+//uses a for loop
+array.sort(function(a, b){
+           return a - b;
+       });
 for (var i = 0; i < array.length; i++) {
   if (array[i]>array[i+1]) {
     console.log(array[i]);
   }
+  else if (typeof array[i+1]==="undefined") {
+    console.log(array[i]);
+  }
 }
 
-//this code puts an array in ascending order
-array.sort(function(a, b){
-           return a - b;
-       });
+//andrei's elegant solution
+function biggestNumberInArray(arr){
+  let highest=0;
+  for (var i = 0; i < arr.length; i++) {
+    if (highest<arr[i]){
+      highest=arr[i];
+    }
+  }
+  return highest;
+}
 
-
+//solving using forEach loop
 function biggestNumberInArray2(arr) {
+  array.sort(function(a, b){
+             return a - b;
+         });
+  array.forEach(func);
 
-}
+  function func (arr){
+    if (array[i]>array[i+1]) {
+      console.log(array[i]);
+    }
+    else if (typeof array[i+1]==="undefined") {
+      console.log(array[i]);
+    }}
 
 function biggestNumberInArray3(arr) {
 
