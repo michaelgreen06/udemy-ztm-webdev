d6c2fabe98a9516c784c627ba4861528fdfa616a
michael green <michaelgreen06@gmail.com>
2022-01-28 23:01:36 +0000

    started working on robofriends component #1

--- section19-react/robofriends/src/index.js
+++ section19-react/robofriends/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import Card from './Card';
+import {robots} from './robots'
 
 ReactDOM.render(
+  //Original code
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <Hello greeting={'hello'+'react ninja'}/>,
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+  </div>,
   document.getElementById('root')
 );
 
