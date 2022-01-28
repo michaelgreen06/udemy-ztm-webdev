a5807b26d918048c38f75ef95e6bf83f84e3c6f1
michael green <michaelgreen06@gmail.com>
2022-01-28 21:21:34 +0000

    made my own comonent!

--- section19-react/robofriends/src/index.js
+++ section19-react/robofriends/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Hello greeting={'hello'+'react ninja'}/>,
   document.getElementById('root')
 );
 
