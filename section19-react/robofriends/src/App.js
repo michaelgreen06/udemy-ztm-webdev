a1ef2e681da0a98335bf02521b712214e3836904
michael green <michaelgreen06@gmail.com>
2022-01-31 18:33:12 +0000

    adding a working search box

---
+++ section19-react/robofriends/src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+
+
+const state={
+
+}
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      robots:robots,
+      searchfield:''
+  }
+}
+  onSearchChange=(event)=>{
+      this.setState({searchfield:event.target.value})
+      const filteredRobots=this.state.robots.filter(robot=>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    console.log(filteredRobots);
+  }
+
+  render(){
+    return(
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={this.state.robots}/>
+      </div>
+    );
+  }
+
+}
+
+export default App;
--- section19-react/robofriends/src/index.js
+++ section19-react/robofriends/src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import Card from './Card';
-import {robots} from './robots'
+import App from './App';
 
 ReactDOM.render(
   //Original code
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-  </div>,
+  <App />,
   document.getElementById('root')
 );
 
