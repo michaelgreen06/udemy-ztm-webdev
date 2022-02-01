d50ecade5491fb1ba89404445f6f7ba4095ea6c8
michael green <michaelgreen06@gmail.com>
2022-02-01 16:33:20 +0000

    adding css and finishing up search

--- section19-react/robofriends/src/App.js
+++ section19-react/robofriends/src/App.js
@@ -2,34 +2,31 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
+import './App.css';
 
 
 
-const state={
-
-}
 class App extends Component{
   constructor(){
     super()
     this.state={
-      robots:robots,
+      robots:[],
       searchfield:''
   }
 }
   onSearchChange=(event)=>{
       this.setState({searchfield:event.target.value})
-      const filteredRobots=this.state.robots.filter(robot=>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-    console.log(filteredRobots);
   }
 
   render(){
+    const filteredRobots=this.state.robots.filter(robots=>{
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+  })
     return(
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={this.state.robots}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
   }
