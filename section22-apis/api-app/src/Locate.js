323ad302f452d94d5ab0ea9002e8de7a71c6d62e
michael green <michaelgreen06@gmail.com>
2022-02-10 19:10:40 +0000

    dove into classes & constructors & they finally make sense! Started building ISS locator

---
+++ section22-apis/api-app/src/Locate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Locate extends React.Component { //Andrei has extends Component. doesn't include React
+  render(){
+    return (
+      <div>
+        <h1>hi world</h1>
+        <p>this is the location of ISIS</p>
+      </div>
+    )
+  }
+}
+
+export default Locate;
--- section22-apis/api-app/src/index.js
+++ section22-apis/api-app/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Locate from './Locate'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Locate />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
