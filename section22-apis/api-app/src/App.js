e595e36cc5ee642733c1d07b0f2e277b820d14e7
michael green <michaelgreen06@gmail.com>
2022-02-18 19:26:52 +0000

    updating description

--- section22-apis/api-app/src/App.js
+++ section22-apis/api-app/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component { //Andrei has extends Component. doesn't incl
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learning to code React
+            Learning to code React and apis
           </a>
         </header>
       </div>
