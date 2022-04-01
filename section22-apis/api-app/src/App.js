a5f504f9608f717b35994c9acee8691f7bdb0e93
michael green <michaelgreen06@gmail.com>
2022-04-01 21:49:53 +0100

    re-visting old lectures after lots of learning

--- section22-apis/api-app/src/App.js
+++ section22-apis/api-app/src/App.js
@@ -29,11 +29,8 @@ import SearchBox from './SearchBox'
 //   }
 // }
 
-//slow and steady is how I'm learning reactjs
-//Hoping I'll be able to make my simple app soon!
-//My Daily green square!
-//I'm not coding but am learning a lot!
-//hopefully next time I start coding it'll be easier because I've done so much learning!
+//Not sure how much more I need to learn before I can make the app I want
+//Do i want to use functional or class components? 
 
 
 class App extends Component{
