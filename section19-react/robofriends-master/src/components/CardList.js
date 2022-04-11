64b12b79dd0c824b3c31f5b26f6673c1a8bb2fb4
michael green <michaelgreen06@gmail.com>
2022-04-11 17:09:33 +0100

    trying to figure out how to re-structure CardList

--- section19-react/robofriends-master/src/components/CardList.js
+++ section19-react/robofriends-master/src/components/CardList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({ robots }) => {
+const CardList = ({robots}) => {
   return (
     <div>
       {
         robots.map((user, i) => {
           return (
             <Card
-              key={i}
+              key={robots.i}
               id={robots[i].id}
               name={robots[i].name}
               email={robots[i].email}
