f6d8c1f15a3669ff1ddf2a164ff86a1caa7471f4
michael green <michaelgreen06@gmail.com>
2022-04-19 19:43:23 +0100

    Finally got app to go to etherscan APIgit add .

--- section22-apis/api-app/src/App.js
+++ section22-apis/api-app/src/App.js
@@ -15,10 +15,12 @@ class App extends Component{
 
 onSearchChange = (event)=>{
 return(
-  fetch(`https://api.etherscan.com/api?module=account&action=tokentx&address=${event.target.value}&startblock=0&endblock=99999999&sort=asc&apikey=S58AX7RGE8H35RT8QXD4RQ2A427RQF7B1M`)
+  window.location.href=`https://api.etherscan.com/api?module=account&action=tokentx&address=${event.target.value}&startblock=0&endblock=99999999&sort=asc&apikey=S58AX7RGE8H35RT8QXD4RQ2A427RQF7B1M`
+  // fetch(`https://api.etherscan.com/api?module=account&action=tokentx&address=${event.target.value}&startblock=0&endblock=99999999&sort=asc&apikey=S58AX7RGE8H35RT8QXD4RQ2A427RQF7B1M`)
 );
   // this.setState({searchfield: event.target.value});
   //wondering if I need to do something else besides fetching to get the new page to come up
+//multisig address=0x230Fc981F7CaE90cFC4ed4c18F7C178B239e5F9F
 }
 
   render(){
