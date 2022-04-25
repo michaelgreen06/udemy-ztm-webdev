cca5e678818cb4ba55bbe8dd2177b06e986cb9c4
michael green <michaelgreen06@gmail.com>
2022-04-26 00:01:59 +0100

    Trying to get particles to work

--- section23/smartbrain/src/App.js
+++ section23/smartbrain/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
+import ParticleShow from './Components/Particles/Particles'
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
@@ -22,10 +22,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-      <Particles
-        params={particlesOptions}
-      />
-      );
+      <ParticleShow />
        <Navigation />
        <Logo />
        <Rank />
