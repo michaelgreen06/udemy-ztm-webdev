b8f3b5c077d60aa411883fa5d294117b03db01bd
michael green <michaelgreen06@gmail.com>
2022-05-04 21:11:18 +0100

    added bounding box!

--- section23/smartbrain/src/App.js
+++ section23/smartbrain/src/App.js
@@ -32,7 +32,26 @@ class App extends Component{
     this.state={
       input:'',
       imageUrl:'',
+      box:{},
     }
+  }
+
+  calcuateFaceLocation=(data)=>{
+    const clarifaiFace=data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image=document.getElementById('inputimage');
+    const width=Number(image.width);
+    const height=Number(image.height);
+    return{
+      leftCol:clarifaiFace.left_col * width,
+      topRow:clarifaiFace.top_row * height,
+      rightCol:width-(clarifaiFace.right_col*width),
+      bottomRow:height-(clarifaiFace.bottom_row*height),
+    }
+  }
+
+  displayFaceBox= (box)=>{
+    console.log(box);
+    this.setState({box:box});
   }
 
   onInputChange=(event)=>{
@@ -45,14 +64,8 @@ onButtonSubmit=()=>{
     .predict(
     Clarifai.FACE_DETECT_MODEL,
     this.state.input)
-    .then(
-    function(response){
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function (err){
-      //there was an error
-    }
-  );
+    .then(response=>this.displayFaceBox(this.calcuateFaceLocation(response)))
+    .catch(err=>console.log(err));
 }
 
   render(){
@@ -68,7 +81,7 @@ onButtonSubmit=()=>{
          onInputChange={this.onInputChange}
          onButtonSubmit={this.onButtonSubmit}
          />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
