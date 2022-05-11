5ffe2af8974f37e1a6d7e3b182b30808ceeebc57
michael green <michaelgreen06@gmail.com>
2022-05-11 23:26:23 +0100

    building a server using expressjs

--- section24/server.js
+++ section24/server.js
@@ -1,15 +1,46 @@
-const http=require('http');
+const express=require('express');
 
-const server = http.createServer((request, response)=>{
-  // console.log('headers',request.headers);
-  console.log('method',request.method);
-  console.log('url',request.url);
+const app = express();
+
+app.get('/',(req, res)=>{
+  res.send('getting root');
+});
+
+app.get('/profile',(req, res)=>{
+  res.send('getting profile');
+});
+
+app.post('/profile',(req, res)=>{
   const user={
-    name:'John',
-    hobby:'golf'
+    name:'Sally',
+    hobby:'GUNS'
   }
-  response.setHeader('Content-Type','application/json');
-  response.end(JSON.stringify(user));
-})
+  res.send(user);
+});
 
-server.listen(3000);
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+// const http=require('http');
+//
+// const server = http.createServer((request, response)=>{
+//   // console.log('headers',request.headers);
+//   console.log('method',request.method);
+//   console.log('url',request.url);
+//   const user={
+//     name:'John',
+//     hobby:'golf'
+//   }
+//   response.setHeader('Content-Type','application/json');
+//   response.end(JSON.stringify(user));
+// })
+//
+// server.listen(3000);
