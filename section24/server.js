53ac4c1c8b1ad863aa3648bb625db5fa1e2805ee
michael green <michaelgreen06@gmail.com>
2022-05-12 16:48:54 +0100

    postman experimentation!

--- section24/server.js
+++ section24/server.js
@@ -21,12 +21,53 @@ app.post('/profile',(req, res)=>{
 app.listen(3000);
 
 
+// const express=require('express');
+//
+// const app = express();
+//
+// app.use((req,res,next)=>{
+//   console.log('<h1>hellooooo!</h1>');
+//   next();
+// })
+//
+// app.get('/',(req, res)=>{
+//   res.send('testest');
+// });
+//
+// app.listen(3000);
+
+
+// OG
+// app.use(bodyparser.urlencoded({extended: false}));
+// app.use(bodyparser.json());
+//
+// New
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
 
 
 
-
-
-
+// const express=require('express');
+//
+// const app = express();
+//
+// app.get('/',(req, res)=>{
+//   res.send('getting root');
+// });
+//
+// app.get('/profile',(req, res)=>{
+//   res.send('getting profile');
+// });
+//
+// app.post('/profile',(req, res)=>{
+//   const user={
+//     name:'Sally',
+//     hobby:'GUNS'
+//   }
+//   res.send(user);
+// });
+//
+// app.listen(3000);
 
 
 // const http=require('http');
