43f03a45a7a1bebf5d55021f26ef3e82b2aa6dfe
michael green <michaelgreen06@gmail.com>
2022-05-25 00:56:40 +0100

    more FS learning

--- section24/script6.js
+++ section24/script6.js
@@ -4,8 +4,30 @@ fs.readFile('./hello.txt',(err,data)=>{
   if (err){
     console.log('errrroooooorrrrr');
   }
-  console.log('1',data.toString('utf8'));
+  console.log('async',data.toString('utf8'));
 })
 
 const file = fs.readFileSync('./hello.txt');
-console.log('2',file.toString());
+console.log('sync',file.toString());
+
+// append
+// fs.appendFile('./hello.txt',' this is so lame!',err=>{
+//   if(err){
+//     console.log(err);
+//   }
+// })
+
+//write
+// fs.writeFile('bye.txt','sad to see you go',err=>{
+//   if (err){
+//     console.log(err);
+//   }
+// })
+
+//delete
+fs.unlink('./bye.txt',err=>{
+  if (err){
+    console.log(err);
+  }
+  console.log('so meta!');
+})
