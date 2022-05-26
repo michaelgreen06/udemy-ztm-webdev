2a9acdcc16748c579336ad6ed6f92df7985ab89d
michael green <michaelgreen06@gmail.com>
2022-05-26 17:11:45 +0100

    How to count the characters in each array?

--- section24/script7.js
+++ section24/script7.js
@@ -12,3 +12,7 @@ console.log('sync',file.toString());
 
 
 //https://stackoverflow.com/questions/881085/count-the-number-of-occurrences-of-a-character-in-a-string-in-javascript
+//when using split() w/ the ( separator it provides arrays of ) characthers.
+//I need to count the number of characters in those arrays then return the sum of them
+//I think I can use an if statement to skip arrays that are less than 1
+//https://stackoverflow.com/questions/52802420/how-to-count-a-number-of-character-in-each-element-of-array-in-js
