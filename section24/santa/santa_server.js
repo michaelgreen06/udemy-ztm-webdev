3e3a552c791faf911415705d36e28b3a3791369d
michael green <michaelgreen06@gmail.com>
2022-06-28 21:31:51 +0100

    using express

--- section24/santa/santa_server.js
+++ section24/santa/santa_server.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app=express();
 
 app.get('/', (req,res)=>{
-  res.send("hiiiii hiii bike to work daaaay 🌊🚲, Next is tube to work day. Then crawl to work day!")
+  res.send("<h1>hiiiii hiii</h1> bike to work daaaay 🌊🚲, Next is tube to work day. Then crawl to work day!")
 });
 
 app.listen(3000);
