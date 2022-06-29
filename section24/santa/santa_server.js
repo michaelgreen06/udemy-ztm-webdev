eb922f361e9d633b16a1063d0f60d9975ce80006
michael green <michaelgreen06@gmail.com>
2022-06-29 22:26:41 +0100

    making a server w/ express to help santa.

--- section24/santa/santa_server.js
+++ section24/santa/santa_server.js
@@ -6,4 +6,6 @@ app.get('/', (req,res)=>{
   res.send("<h1>hiiiii hiii</h1> bike to work daaaay 🌊🚲, Next is tube to work day. Then crawl to work day!")
 });
 
+//I'm definitely going to make a server today. At least I'll try that is.
+
 app.listen(3000);
