412997c36504fcc1effd40b27f3427e7a734bad5
michael green <michaelgreen06@gmail.com>
2022-07-16 01:22:14 +0100

    eliminated trailing white space to solve prob 1

--- section24b/andrei_solution/script.js
+++ section24b/andrei_solution/script.js
@@ -5,12 +5,12 @@ const fs=require('fs');
 function question1(){
   fs.readFile('./santa.txt', (err,data)=>{
     const directions=data.toString();
-    const directionsArray=directions.split('');
+    const directionsArray=directions.split("");
     const answer=directionsArray.reduce((acc,currentValue)=>{
-      if (currentValue==='('){
-        return acc+=1
-      }else if (currentValue===')'){
-        return acc-=1
+      if (currentValue==="("){
+        return acc += 1
+      }else if (currentValue===")"){
+        return acc -= 1
       }
     },0)
     console.log(answer);
