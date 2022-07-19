9741d873785aa8596a10bb4092f7be6a5afa6b96
michael green <michaelgreen06@gmail.com>
2022-07-19 02:17:05 +0100

    finished section 24. Starting section25

--- section24b/andrei_solution/script.js
+++ section24b/andrei_solution/script.js
@@ -4,6 +4,7 @@ const fs=require('fs');
 // ) santa goes down 1 floor
 function question1(){
   fs.readFile('./santa.txt', (err,data)=>{
+    console.time('q1-santa-time');
     const directions=data.toString();
     const directionsArray=directions.split("");
     const answer=directionsArray.reduce((acc,currentValue)=>{
@@ -13,9 +14,38 @@ function question1(){
         return acc -= 1
       }
     },0)
-    console.log(answer);
+    console.log("floor",answer);
+    console.timeEnd('q1-santa-time');
   })
 }
 
-question1();
+ question1();
+
 //2 - position of first character that causes santa to enter basement?
+
+function question2(){
+  fs.readFile('./santa.txt',(err,data)=>{
+    console.time('q2-santa-time');
+    const directions=data.toString();
+    const directionsArray=directions.split("");
+    let accumulator =0;
+    let counter = 0;
+    const answer=directionsArray.some((currentItem)=>{
+      if (currentItem==="("){
+        accumulator += 1
+      }else if (currentItem===")"){
+        accumulator -= 1
+      }
+      counter ++;
+      return accumulator <0;
+    })
+    console.timeEnd('q2-santa-time');
+    console.log("basement entered at inst #:",counter);
+  })
+}
+
+
+question2();
+
+
+//
