7d66ebf0707439e33c1409b7207a9ad0b35d965f
michael green <michaelgreen06@gmail.com>
2022-07-21 00:50:37 +0100

    added signin route

--- smartbrain/smartbrain-api/server.js
+++ smartbrain/smartbrain-api/server.js
@@ -2,8 +2,40 @@ const express=require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const database={
+  users:[
+    {
+      id:'123',
+      name:'mike',
+      email:'mike@gmail.com',
+      password:'cookies',
+      entries:0,
+      joined:new Date()
+    },
+    {
+      id:'124',
+      name:'sally',
+      email:'sally@gmail.com',
+      password:'bananas',
+      entries:0,
+      joined:new Date()
+    }
+  ]
+}
+
 app.get('/',(req,res)=>{
   res.send('this is working');
+})
+
+app.post('/signin', (req,res)=>{
+  if(req.body.email===database.users[0].email&&
+  req.body.password===database.users[0].password){
+    res.json('success!');
+  }else{
+    res.status(400).json('error logging in');
+  }
 })
 
 app.listen(3000, ()=>{
