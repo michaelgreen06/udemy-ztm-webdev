76eae88af493854b5ca5e054f9ac6ff2e83fc20f
michael green <michaelgreen06@gmail.com>
2022-07-26 02:50:26 +0100

    storing passwords securely with bcrypt

--- smartbrain/smartbrain-api/server.js
+++ smartbrain/smartbrain-api/server.js
@@ -1,6 +1,6 @@
 const express=require('express');
-
 const app = express();
+const bcrypt=require('bcrypt-nodejs');
 
 app.use(express.json());
 
@@ -10,7 +10,6 @@ const database={
       id:'123',
       name:'mike',
       email:'mike@gmail.com',
-      password:'cookies',
       entries:0,
       joined:new Date()
     },
@@ -18,9 +17,15 @@ const database={
       id:'124',
       name:'sally',
       email:'sally@gmail.com',
-      password:'bananas',
       entries:0,
       joined:new Date()
+    }
+  ],
+  login:[
+    {
+      id:'987',
+      hash:'',
+      email:'mike@gmail.com'
     }
   ]
 }
@@ -30,6 +35,12 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/signin', (req,res)=>{
+  bcrypt.compare("farts", '$2a$10$FXq5gdEfaQ2pztfTASrdZeT89Zig.PAFQmA6SZnb/cZRkKJ68dIba', function(err, res) {
+      console.log('first guess',res);
+  });
+  bcrypt.compare("veggies", '$2a$10$FXq5gdEfaQ2pztfTASrdZeT89Zig.PAFQmA6SZnb/cZRkKJ68dIba', function(err, res) {
+      console.log('2nd guess',res);
+  });
   if(req.body.email===database.users[0].email&&
   req.body.password===database.users[0].password){
     res.json('success!');
@@ -64,6 +75,30 @@ app.get('/profile/:id',(req,res)=>{
     res.status(400).json('user not found');
   }
 })
+
+app.put('/image', (req,res)=>{
+  const {id}=req.body;
+  let found=false;
+  database.users.forEach(user=>{
+    if (user.id===id){
+      found=true;
+      user.entries++;
+    return  res.json(user.entries);
+    }
+  })
+  if (!found){
+    res.status(400).json('user not found');
+  }
+})
+
+
+// bcrypt.hash(password, null, null, function(err, hash) {
+//   console.log(hash);
+// });
+
+// Load hash from your password DB.
+
+
 
 app.listen(3000, ()=>{
   console.log('app is running on port 3000');
