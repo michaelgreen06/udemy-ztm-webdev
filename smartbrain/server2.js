a1d5bfd2917cc28bf0a85af1b1d6fdbdeda564dd
michael green <michaelgreen06@gmail.com>
2022-09-21 23:37:20 +0100

    working on deploying to heroku

--- smartbrain/server2.js
+++ smartbrain/server2.js
@@ -2,6 +2,7 @@ const app=require('http')
   .createServer((req,res)=>res.send('oh hi there!'));
 
 const Port=process.env.Port;
+//Working on deploying to heroku production env
 
 app.listen(Port,()=>{
   console.log(`server is listening on port ${Port}`);
