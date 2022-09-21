f8ddb801de8a76536467b502f19865496accec3d
michael green <michaelgreen06@gmail.com>
2022-09-21 01:21:29 +0100

    figured out my problem! Was using quote marks intead of backticks!

--- smartbrain/server2.js
+++ smartbrain/server2.js
@@ -1,10 +1,10 @@
 const app=require('http')
   .createServer((req,res)=>res.send('oh hi there!'));
 
-const Port=process.env.Port
+const Port=process.env.Port;
 
 app.listen(Port,()=>{
-  console.log('server is listening on ${Port}');
+  console.log(`server is listening on port ${Port}`);
 });
 
 console.log(Port);
