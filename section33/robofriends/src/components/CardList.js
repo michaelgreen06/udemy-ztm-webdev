ffdbfb1585b6a7d011b25461208fc789ab3b10bf
Michael Green <michaelgreen06@gmail.com>
2022-11-01 19:15:55 -0600

    updating robofrineds to learn redux

---
+++ section33/robofriends/src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = (props) => {
+  return (
+    <div>
+      {
+        props.robots.map((user, i) => {
+          return (
+            <Card
+              key={props.robots[i].id}
+              id={props.robots[i].id}
+              name={props.robots[i].name}
+              email={props.robots[i].email}
+              city={props.robots[i].address.city}
+              />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default CardList;
+
+// comments so I can get a green square!!
