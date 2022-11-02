2817042c4c67cf54fa1bfef832a4ecb8dedbbef0
Michael Green <michaelgreen06@gmail.com>
2022-11-02 16:20:25 -0600

    working on adding reducers & actions

---
+++ section33/robofriends/src/reducers.js
@@ -0,0 +1,14 @@
+import {CHANGE_SEARCH_FIELD} from './constants.js'
+const initialState={
+  searchfield: ''
+}
+
+export const searchRobots=(state=initialState,action={})=>{
+  switch(action.type){
+    case:CHANGE_SEARCH_FIELD:
+      return Object.assign({},state,searchfield:action.payload)
+      //return {...state,searchfield:action.payload} <- this is what the above looks like when destructured
+    default:
+      return state;
+  }
+}
