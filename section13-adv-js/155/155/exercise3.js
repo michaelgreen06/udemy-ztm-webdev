b4061414627f58b8a175f71eb46866978c49dbaa
Michael Green <michaelgreen06@gmail.com>
2023-07-18 14:13:36 -0600

    finsihed 157 exercises

--- section13-adv-js/155/155/exercise3.js
+++ section13-adv-js/155/155/exercise3.js
@@ -22,21 +22,17 @@ var eyeColor = person.eyeColor;
 var { firstName, lastName, age, eyeColor } = person;
 
 // Object properties
-var a = "test";
-var b = true;
-var c = 789;
-
-const obj = {
-  [a]: "test",
-  [b]: true,
-  [c]: 789,
-};
+const a = "test";
+const b = true;
+const c = 789;
 
 var okObj = {
   a: a,
   b: b,
   c: c,
 };
+
+const okObj = { a, b, c };
 
 // Template strings
 var message =
@@ -46,14 +42,18 @@ var message =
   city +
   " last summer no???";
 
+const message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer, no??`;
+
 // default arguments
 // default age to 10;
-function isValidAge(age) {
+function isValidAge(age = 10) {
   return age;
 }
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
+const sym1 = Symbol();
+const sym2 = Symbol("michael0");
 
 // Arrow functions
 function whereAmI(username, location) {
@@ -63,3 +63,11 @@ function whereAmI(username, location) {
     return "I am totally lost!";
   }
 }
+
+const whereAmI = (username, location) => {
+  if (username && location) {
+    return "I am not lost";
+  } else {
+    return "I am totally lost!";
+  }
+};
