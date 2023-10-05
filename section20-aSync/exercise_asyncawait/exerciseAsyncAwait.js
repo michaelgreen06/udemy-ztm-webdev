ebe119525f9cfe237f6f344b23558b3b99a5cbca
Michael Green <michaelgreen06@gmail.com>
2023-10-05 10:41:22 -0600

    working on understanding async await syntax with promises'

--- section20-aSync/exercise_asyncawait/exerciseAsyncAwait.js
+++ section20-aSync/exercise_asyncawait/exerciseAsyncAwait.js
@@ -5,11 +5,11 @@ fetch("https://jsonplaceholder.typicode.com/users/")
   .then((response) => response.json())
   .then(console.log);
 
-  async function getData2(){
-  const userData=await fetch("https://jsonplaceholder.typicode.com/users/")
-  const response1=await userData.json();
+async function getData2() {
+  const userData = await fetch("https://jsonplaceholder.typicode.com/users/");
+  const response1 = await userData.json();
   console.log(response1);
-  }
+}
 
 const getData1 = async function () {
   const response = await fetch("https://jsonplaceholder.typicode.com/users/");
@@ -24,7 +24,7 @@ const urlss = [
   "https://jsonplaceholder.typicode.com/albums",
 ];
 
-Promise.all(urlss.map((url) => fetch(url).then((resp = resp.json()))))
+Promise.all(urlss.map((url) => fetch(url).then((resp) => resp.json())))
   .then((array) => {
     console.log("users", array[0]);
     console.log("posts", array[1]);
@@ -32,21 +32,33 @@ Promise.all(urlss.map((url) => fetch(url).then((resp = resp.json()))))
   })
   .catch("oops");
 
-async function fetchData(){
-  const resp = await fetch(())
-  const data = await resp.json()
-  console.log(data);
+//this fetchData1 function works!
+//next I could destructure the array
+async function fetchData1() {
+  const data2 = await Promise.all(
+    urlss.map((url) => fetch(url).then((resp) => resp.json()))
+  );
+  const users = data2[0];
+  const posts = data2[1];
+  const albums = data2[2];
+
+  console.log(data2);
 }
 
 //this is function that I want to run when I want to get all resolved promises at the same time
 //1)understand - I'm not sure I understand what is happening here. I think I am trying to create a way
 //to wait for all promises to be resolved so I can log everything to the console at the same time
-//I am fetching data from 3 different APIs and need to wait for each to be resolved
-//
+//I am fetching data from 3 different APIs and need to wait for each to be resolved. I want to do the first
+//version the stupid simple way w/o array destructurring. Also, an easier way to start would be to do it
+//for one url instead of all 3.
+//The async function works for one of the URLs in the array. How can I get it to work for all elements in the array?
+//I need some type of loop. I could use a for loop. Andrei would say I should use map. How would it work if I use map?
+//map would make a new array of arrays that each contain objects.
+//I obviously wouldn't write the function this way but I'm just trying to do small chunks at a time so I can understand
+//what is happening.
+//promise.all receives an array of promises and returns an array of fulfilled promises
 //2)plan
 //3)divide
-
-
 
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
