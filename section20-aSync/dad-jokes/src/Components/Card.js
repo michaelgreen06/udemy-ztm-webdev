5301d1bff92f9493b709fb457d9d85c8c26df2cc
Michael Green <michaelgreen06@gmail.com>
2023-11-14 10:50:34 +0700

    figured out how to fetch DADa

--- section20-aSync/dad-jokes/src/Components/Card.js
+++ section20-aSync/dad-jokes/src/Components/Card.js
@@ -31,16 +31,6 @@ const urls = createApiArray();
 //getting each one to resolve individually is currently too complex for me so I will just use promise.all and wait
 //until all are settlted
 
-Promise.all(
-  urls.map(
-    setTimeout((url) => {
-      return fetch(url).then((resp) => resp.json());
-    }, 50)
-  )
-).then((array) => {
-  console.log(array[0]);
-});
-
 //Need to understand why chat GPT suggested this code:
 //specifically what is new Proimse and why are we using it?!
 
@@ -59,63 +49,12 @@ Promise.all(
   console.log(array[0]);
 });
 
-//trying to remember where I left off and what I need to do next. I created an array of APIs that need to be called
-//to fill each card.
-//always return the result of .then() so there is something for the next .then() in the chain to accept
-
-//10/18/23 re-watch async videos and try to understand the above code from chat GPT
 //I think I understand what is happening w/ the chat GPT code. We had to create a new promise w/
 //fetch inside of it so that we can control when fetch is called. If we don't wrap fetch in a promise it will
 //execute right away. We use the resolve argument in the callback function of the promise constructor to move
 //the promise into the fulfilled state.
 
-//My try w/o help
-
-Promise.all(
-  urls.map(
-    setTimeout((url) => {
-      return fetch(url).then((resp) => resp.json());
-    }, 50)
-  )
-).then((array) => {
-  console.log(array[0]);
-});
-
-Promise.all(
-  urls.map((url) => {
-    return new Promise((resolve) => {
-      setTimeout((url) => {
-        fetch(url)
-          .then((resp) => resp.json())
-          .then((data) => resolve(data))
-          .then((error) => resolve(error));
-      }, 50);
-    });
-  })
-);
-
-//10/19/23
-//rewrite the promise.all function entirely from scratch!!
-//1145 I am trying to get a new array that makes fetch calls after 50ms each
-//I have an array of urls and I want one URL to get called then wait 50 ms and fetch the next URL
-//do I need to declare this as a variable? Not sure.
-
-Promise.all(
-  urls.map((url) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        fetch(url)
-          .then((resp) => resp.json())
-          .then((data) => resolve(data))
-          .catch((error) => resolve(error));
-      }, 50);
-    });
-  })
-);
-
-//11/13/23 rewrite the Promise.all statement from scratch again
-
-//promise.all takes an iterable of promises & //returns a single promise
+//promise.all takes an iterable of promises & returns a single promise
 //w/ a resulf from all the promises that were passed to it
 //I want to pass the iterable of promises that are fetch calls to the icanhazdadjoke api.
 //I know i need to make a new promise. I have an array of urls and I want to use map to call them one at a time
@@ -127,7 +66,27 @@ Promise.all(
 //creating a variable to define the array of promises that will be passed into promise.all because it could be easier
 
 const promiseArray = urls.map((url) => {
-  new Promise(fetch(url));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      fetch(url, { headers: { Accept: "application/json" } })
+        .then((resp) => resp.json())
+        .then((data) => resolve(data))
+        .catch((error) => console.error(error));
+    }, 50);
+  });
 });
 
 //11/14/23 - figure out how to complete the above promiseArray variable. needs timeout and resolve,reject
+//final form will be promise.all(promiseArray)
+//I believe promiseArray will return an array of promisess then I can feed that into promise.all
+Promise.all(promiseArray).then((array) => console.log(array[1]));
+
+//so the above code works which is great! But now I know that it doesn't work great w/ icanhazdadjoke because
+//it returns headers etc instead of straight json and now I need to figure out what to do about that.
+//I now know I need to add an object to my fetch request. The object has multiple key value pairs: method, headers
+//the headers value is an object that contains keys of: User-Agent, Accept, Authorization, Content-Type
+
+//GREAT!! I have learned and made progress! and I have seen that my method will display different jokes from the DB
+//Now I need to figure out how this new information is used in my project.
+//I think that the functions to call the API should be located in the App component and should useEffect to get the data
+//
--- section20-aSync/dad-jokes/src/Components/CardList.js
+++ section20-aSync/dad-jokes/src/Components/CardList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = () => {
+const CardList = ({}) => {
   const jokeData = () => {
     const array = [];
     for (let i = 1; i < 11; i = i + 1) {
@@ -9,6 +9,7 @@ const CardList = () => {
     }
     return array;
   };
+  //I believe I need to change this to be an array of jokes from icanhazdadjoke instead of the random array above
   const data = jokeData();
   return (
     <div>
@@ -28,3 +29,8 @@ const CardList = () => {
 export default CardList;
 
 //I know I need an array of cards and that I need to use map to get an array of the cards.
+//I think I will get rid of the jokeData array I made above because that was just to test to see if my display was going to work
+//now that I know I have this setup properly for my display to work I can use real jokes in the cardList
+
+//11/15/23 use useEffect to fetch data from icanhazdadjoke and get an array of jokes then feed those jokes into each card using map
+//The current api responses include joke ID
