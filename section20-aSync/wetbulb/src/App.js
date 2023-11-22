d65200d4f661cb4b80d633b2617dbb72a047a77e
Michael Green <michaelgreen06@gmail.com>
2023-11-22 11:42:44 +0700

    integrating openweathermap api

--- section20-aSync/wetbulb/src/App.js
+++ section20-aSync/wetbulb/src/App.js
@@ -1,33 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [location, setLocation] = useState({});
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(
+    function fetchWeather() {
+      if (latitude !== "" && longitude !== "") {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dfd00a0b3921425262c44a3a2c7508ad`
+        )
+          .then((resp) => resp.json())
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => console.error(error));
+      }
+      fetchWeather();
+    },
+    [longitude, latitude]
+  );
 
   function locate() {
-    navigator.geolocation.getCurrentPosition(success, error);
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
   }
 
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(`Your latitude is:${latitude}. Your longitude is:${longitude}`);
-  }
+  useEffect(() => {
+    locate()
+      .then((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      })
+      .catch((error) => console.error("failed to get location: ", error));
+  });
 
-  function error() {
-    console.log("Geolocation is not enabled");
-  }
-
-  locate();
-
-  return <div></div>;
+  return (
+    <div>
+      <h1>your latitude is: {latitude}</h1>
+      <h1>your longitude is: {longitude}</h1>
+    </div>
+  );
 }
 
 export default App;
 
-//I need to find the formula that I will use to calculate the wet bulb temp. I need to know the users location
-//I need to feed the user's location into the openweather.org api to get the info that goes into the equation
-//looks like i run navigator.geolocation to find the users position. YES! I actually was able to use the API to find
-//my location successfully. Now I have my location. I easily get it by running navigator.geolocation etc
-//the geolocation method(?) returns an object that contains the lat & long which I then need to plug into the
-//open weather api to determine the current conditions. One quesiton is: when do I want to get the user's location?
-//Another is: do I need to use promises here for this aSync operation of getting the user's location?
+//11/23/23 Create a solid plan of what I want to happen before I start trying to code it up
