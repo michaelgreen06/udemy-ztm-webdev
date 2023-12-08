2d033163b53ecd157d8b327b1d41e767e9c265a8
Michael Green <michaelgreen06@gmail.com>
2023-12-08 10:51:54 +0700

    learning about useRef for implementing autocomplete searchbox

--- section20-aSync/wetbulb/src/Components/SearchBox.js
+++ section20-aSync/wetbulb/src/Components/SearchBox.js
@@ -1,59 +1,42 @@
-//code from chatGPT using autocomplete component
+//new code from Akash example
 
-import React, { useState } from "react";
-import { useLoadScript, Autocomplete } from "@react-google-maps/api";
+import React, { useRef } from "react";
+import { StandaloneSearchBox, useJsApiLoader } from "@react-google-maps/api";
+const libraries = ["places"];
+const PlaceComponent = () => {
+  const inputRef = useRef();
 
-function SearchBox({ onLocationSelect }) {
-  const [query, setQuery] = useState("");
-  // const [address, setAddress] = useState("");
-  // const [location, setLocation] = useState({ lat: null, lng: null });
-  const placesAPIKey = process.env.REACT_APP_G_PLACES_API_KEY;
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: placesAPIKey,
-    libraries: ["places"],
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    libraries,
   });
 
-  const handleInput = (e) => {
-    setQuery(e.target.value);
+  const handlePlaceChanged = () => {
+    const [place] = inputRef.current.getPlaces();
+    if (place) {
+      console.log(place.formatted_address);
+      console.log(place.geometry.location.lat());
+      console.log(place.geometry.location.lng());
+    }
   };
-
-  const handleSelect = (place) => {
-    // setAddress(place.name);
-    const lat = place.geometry.location.lat();
-    const lng = place.geometry.location.lng();
-    console.log("lat: ", lat, "lng: ", lng);
-    // setLocation({ lat, lng });
-    onLocationSelect(lat, lng);
-  };
-
-  if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    <div>
-      <Autocomplete onSelect={(place) => handleSelect(place)}>
-        {/* chatGPT said the prop for autocomplete should actually be onPlaceChanged but when I change it I get errors */}
+    isLoaded && (
+      <StandaloneSearchBox
+        onLoad={(ref) => (inputRef.current = ref)}
+        onPlacesChanged={handlePlaceChanged}
+      >
         <input
           type="text"
-          placeholder="Search for a location"
-          value={query}
-          onChange={handleInput}
+          className="form-control"
+          placeholder="Enter Location"
         />
-      </Autocomplete>
-      <div>
-        {/* {address && (
-          <div>
-            <p>Selected Address: {address}</p>
-            <p>Latitude: {location.lat}</p>
-            <p>Longitude: {location.lng}</p>
-          </div>
-        )} */}
-      </div>
-    </div>
+      </StandaloneSearchBox>
+    )
   );
-}
+};
 
-export default SearchBox;
+export default PlaceComponent;
 
 //12/5/23 look into using the built in AutoComplete component instead of using this approacth
 
@@ -62,6 +45,7 @@ export default SearchBox;
 //@react-google-maps/api docs on github: https://github.com/JustFly1984/react-google-maps-api/tree/masterç
 //google places api library: https://developers.google.com/maps/documentation/javascript/places#place_details
 //google places api docs: https://developers.google.com/maps/documentation/places/web-service/overview
+//helpful example: https://www.ultimateakash.com/blog-details/Ii0zJGAKYAo=/How-To-Integrate-Google-Places-Autocomplete-in-React-2022
 
 //I might just be lazy and use the code chatGPT suggested so I can move on w/ this project. I am not trying to learn
 //everything in depth all at once. I want to keep learning other things as well. I have the searchbox showing up in my app
@@ -71,3 +55,60 @@ export default SearchBox;
 //there. I will also add it to the main page that shows if the user allows location but may want to check another location
 //When the search box state changes, by a user selecting a place, then there will be a useEffect that runs to fetch the weather
 //data for that location which will determine the WBT. How do I get the lat & long from the autocomplete selection?
+
+//code from chatGPT using autocomplete component
+
+// import React, { useState } from "react";
+// import { useLoadScript, Autocomplete } from "@react-google-maps/api";
+
+// function SearchBox({ onLocationSelect }) {
+//   const [query, setQuery] = useState("");
+//   // const [address, setAddress] = useState("");
+//   // const [location, setLocation] = useState({ lat: null, lng: null });
+//   const placesAPIKey = process.env.REACT_APP_G_PLACES_API_KEY;
+
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: placesAPIKey,
+//     libraries: ["places"],
+//   });
+
+//   const handleInput = (e) => {
+//     setQuery(e.target.value);
+//   };
+
+//   const handleSelect = (place) => {
+//     // setAddress(place.name);
+//     const lat = place.geometry.location.lat();
+//     const lng = place.geometry.location.lng();
+//     console.log("lat: ", lat, "lng: ", lng);
+//     // setLocation({ lat, lng });
+//     onLocationSelect(lat, lng);
+//   };
+
+//   if (!isLoaded) return <div>Loading...</div>;
+
+//   return (
+//     <div>
+//       <Autocomplete onSelect={(place) => handleSelect(place)}>
+//         {/* chatGPT said the prop for autocomplete should actually be onPlaceChanged but when I change it I get errors */}
+//         <input
+//           type="text"
+//           placeholder="Search for a location"
+//           value={query}
+//           onChange={handleInput}
+//         />
+//       </Autocomplete>
+//       <div>
+//         {/* {address && (
+//           <div>
+//             <p>Selected Address: {address}</p>
+//             <p>Latitude: {location.lat}</p>
+//             <p>Longitude: {location.lng}</p>
+//           </div>
+//         )} */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SearchBox;
