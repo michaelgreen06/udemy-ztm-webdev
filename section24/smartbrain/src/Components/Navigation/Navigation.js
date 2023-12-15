1c5c5ebbb1bdd8e4598d7e4e320334a5a6ed0474
Michael Green <michaelgreen06@gmail.com>
2023-12-15 12:01:25 +0700

    started smartbrain project

---
+++ section24/smartbrain/src/Components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p className="f3 link dim black pa3 underline pointer">sign out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
