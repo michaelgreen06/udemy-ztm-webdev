d74ab648e079f4c959171b3941de1736cb04060c
Michael Green <michaelgreen06@gmail.com>
2023-12-20 11:24:27 +0700

    starting to add functionality

--- section24/smartbrain/src/App.js
+++ section24/smartbrain/src/App.js
@@ -3,10 +3,19 @@ import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLink from "./Components/ImageLink/ImageLink";
 import Rank from "./Components/Rank/Rank";
+import ParticlesBg from "particles-bg";
+
+const particleSettings = {
+  className: "particles",
+  color: "#ffffff",
+  type: "cobweb",
+  bg: true,
+};
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg {...particleSettings} />
       <Navigation />
       <Logo />
       <Rank />
