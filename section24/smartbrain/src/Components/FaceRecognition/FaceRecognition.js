6fbe390f8af7283819e22142ded3d58417b63126
Michael Green <michaelgreen06@gmail.com>
2024-01-24 14:49:58 +0700

    fixed conditional rendering

--- section24/smartbrain/src/Components/FaceRecognition/FaceRecognition.js
+++ section24/smartbrain/src/Components/FaceRecognition/FaceRecognition.js
@@ -2,42 +2,46 @@ import React from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageSrc, dotPos }) => {
-  
-  return ({imageSrc==""?<div><p>input your image</p></div>:
-  <div className="center ma">
-  <div className="absolute mt2">
-    <img
-      id="inputImage"
-      src={imageSrc}
-      alt="Girl in a jacket"
-      width="500px"
-      height="auto"
-    />
-    <div
-      id="topLeft"
-      className="dot"
-      style={{ top: `${dotPos.topY}px`, left: `${dotPos.leftX}px` }}
-    ></div>
-    <div
-      id="topRight"
-      className="dot"
-      style={{ top: `${dotPos.topY}px`, left: `${dotPos.rightX}px` }}
-    ></div>
-    <div
-      id="bottomLeft"
-      className="dot"
-      style={{ top: `${dotPos.bottomY}px`, left: `${dotPos.leftX}px` }}
-    ></div>
-    <div
-      id="bottomRight"
-      className="dot"
-      style={{ top: `${dotPos.bottomY}px`, left: `${dotPos.rightX}px` }}
-    ></div>
-  </div>
-  <div className="bounding-box"></div>
-</div>
-}
-   
+  return (
+    <div className="center ma">
+      {imageSrc ? (
+        <div className="absolute mt2">
+          <img
+            id="inputImage"
+            src={imageSrc}
+            alt="Analyzed Image"
+            width="500px"
+            height="auto"
+          />
+          {Object.keys(dotPos).length > 0 && (
+            <>
+              <div
+                id="topLeft"
+                className="dot"
+                style={{ top: `${dotPos.topY}px`, left: `${dotPos.leftX}px` }}
+              ></div>
+              <div
+                id="topRight"
+                className="dot"
+                style={{ top: `${dotPos.topY}px`, left: `${dotPos.rightX}px` }}
+              ></div>
+              <div
+                id="bottomLeft"
+                className="dot"
+                style={{ top: `${dotPos.bottomY}px`, left: `${dotPos.leftX}px` }}
+              ></div>
+              <div
+                id="bottomRight"
+                className="dot"
+                style={{ top: `${dotPos.bottomY}px`, left: `${dotPos.rightX}px` }}
+              ></div>
+            </>
+          )}
+        </div>
+      ) : (
+        <p>please input an image URL</p>
+      )}
+    </div>
   );
 };
 
